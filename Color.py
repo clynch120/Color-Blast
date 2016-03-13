d0d86fc0ea2273fdf0b190e597a83dba6648494f
Charles Lynch <charlesloper2001@gmail.com>
2016-03-13 10:26:30 -0700

    Added files via upload

--- Color.py
+++ Color.py
@@ -1,8 +1,20 @@
 import random as ra
+import time
 
 
 class Color:
+    point = 0
 
-    def whatWhat(self):
-        btn_click = []
-        return btn_click
+    def __init__(self):
+        print("hello")
+
+    def random_pattern(self):
+        rand = ra.randint(4)
+        return rand
+
+    def user_input(self):
+        global point
+        if self.user_input() == self.random_pattern():
+            point += 1
+            return point
+
--- Gui.py
+++ Gui.py
@@ -3,9 +3,9 @@ from GUI.Color import Color
 
 
 class Gui(Frame, Color):
-
+    # calls all the methods when the program starts
     def __init__(self):
-        Frame.__init__(self, bg="#303030")
+        Frame.__init__(self, bg="#303030")  # set window background color
         self.window()
         self.red_btn()
         self.green_btn()
@@ -13,41 +13,40 @@ class Gui(Frame, Color):
         self.yellow_btn()
         self.grid()
 
-    def window(self):
+    def window(self):    # creates the window
         root.title("Color Blast")
         root.geometry("950x840")
         root.config(bg="#303030")
 
-    def red_btn(self):
+    def red_btn(self):    # creates the red button
         red = Button(self, bg="red")
         red.config(width=50, height=20)
         red["text"] = "Red"
-        red["command"] = self.whatWhat()
-        red.grid(row=0, column=0, padx=70, pady=70)
+        red["command"] = self.quit()
+        red.grid(row=0, column=0, padx=70, pady=70)    # sets location and space between buttons
 
-    def blue_btn(self):
+    def blue_btn(self):    # creates the blue button
         blue = Button(self, bg="blue")
         blue.config(width=50, height=20)
         blue["text"] = "Blue"
-        blue["command"] = self.whatWhat()
+        blue["command"] = self.quit()
         blue.grid(row=2, column=0)
 
-    def green_btn(self):
+    def green_btn(self):    # creates the green button
         green = Button(self, bg="green")
         green.config(width=50, height=20)
         green["text"] = "Green"
-        green["command"] = self.whatWhat()
+        green["command"] = self.quit()
         green.grid(row=0, column=2)
 
-    def yellow_btn(self):
+    def yellow_btn(self):    # creates the yellow button
         yellow = Button(self, bg="yellow")
         yellow.config(width=50, height=20)
         yellow["text"] = "Yellow"
-        yellow["command"] = self.whatWhat()
+        yellow["command"] = self.quit()
         yellow.grid(row=2, column=2)
 
 
 root = Tk()
 app = Gui()
 app.mainloop()
-print(Color.whatWhat)
