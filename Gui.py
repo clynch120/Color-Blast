1d8b4738f5554260f6f3fa39fb3fdff4434b5192
Charles Lynch <charlesloper2001@gmail.com>
2016-03-14 15:17:45 -0700

    Added files via upload

--- Gui.py
+++ Gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
-#from GUI.Color import Color
+import random as ra
+import time
 
 
 class Gui(Frame):
@@ -7,11 +8,27 @@ class Gui(Frame):
     def __init__(self):
         Frame.__init__(self, bg="#303030")  # set window background color
         self.window()
+        self.level()
         self.red_btn()
         self.green_btn()
         self.blue_btn()
         self.yellow_btn()
         self.grid()
+        self.count = 0
+        self.user_input = []
+        self.com_list = []
+
+    def level(self):    # creates pop up window
+        top = Toplevel()
+        top.attributes("-topmost", 1)
+        top.title("Difficultly level")
+        top.geometry("250x100")
+        msg = Message(top, text="Enter Difficultly : 1 - 9", width=150)
+        msg.pack()
+        e = Entry(top)
+        e.pack()
+        button = Button(top, text="Select", command=top.destroy)
+        button.pack()
 
     def window(self):    # creates the window
         root.title("Color Blast")
@@ -19,34 +36,55 @@ class Gui(Frame):
         root.config(bg="#303030")
 
     def red_btn(self):    # creates the red button
-        red = Button(self, bg="red")
+        red = Button(self, bg="red", command=self.count_red)
         red.config(width=50, height=20)
         red["text"] = "Red"
-        red["command"] = self.quit()
         red.grid(row=0, column=0, padx=70, pady=70)    # sets location and space between buttons
 
     def blue_btn(self):    # creates the blue button
-        blue = Button(self, bg="blue")
+        blue = Button(self, bg="blue", command=self.count_blue)
         blue.config(width=50, height=20)
         blue["text"] = "Blue"
-        blue["command"] = self.quit()
         blue.grid(row=2, column=0)
 
     def green_btn(self):    # creates the green button
-        green = Button(self, bg="green")
+        green = Button(self, bg="green", command=self.count_green)
         green.config(width=50, height=20)
         green["text"] = "Green"
-        green["command"] = self.quit()
         green.grid(row=0, column=2)
 
     def yellow_btn(self):    # creates the yellow button
-        yellow = Button(self, bg="yellow")
+        yellow = Button(self, bg="yellow", command=self.count_yellow)
         yellow.config(width=50, height=20)
         yellow["text"] = "Yellow"
-        yellow["command"] = self.quit()
         yellow.grid(row=2, column=2)
 
+    def count_red(self):
+        r = self.user_input.append(1)
+        return r
 
+    def count_blue(self):
+        b = self.user_input.append(2)
+        return b
+
+    def count_green(self):
+        g = self.user_input.append(3)
+        return g
+
+    def count_yellow(self):
+        y = self.user_input.append(4)
+        print(self.user_input)
+        return y
+
+    def computer_list(self):
+        while self.user_input == self.com_list:
+            com = self.com_list.append(ra.randint(1, 4))
+            time.sleep(1)
+            return com
+
+    def show_com_list(self):
+        
 root = Tk()
 app = Gui()
 app.mainloop()
+
